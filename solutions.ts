7c3287f003b92788926f7db993087e99e470d708
Imtiaz Hossain <imtiazhossain231@gmail.com>
2026-05-08 22:41:41 +0600

    Add files via upload

---
+++ solutions.ts
@@ -0,0 +1,12 @@
+// const course: string  = "Next Level Web Developent";
+
+// console.log(course)
+
+// // This prints a message to the terminal or browser console
+// // console.log("Hello, JavaScript!");
+// Variables with explicit types
+let userName: string = "Alice";
+let userAge: number = 22;
+let isStudent: boolean = true;
+
+console.log(`${userName} is ${userAge} years old.`);
